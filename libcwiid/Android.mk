591fe5bb8c0a4eac3ce63e7a8069a86de78e9e28
cvpcs <root@cvpcs.org>
2010-05-13 16:03:01 -0500

    Added an android makefile

---
+++ libcwiid/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:= \
+	bluetooth.c command.c \
+	connect.c interface.c \
+	process.c state.c \
+	thread.c util.c
+LOCAL_C_INCLUDES += \
+	external/bluetooth/bluez/include
+LOCAL_CFLAGS += \
+	-DPTHREAD_CANCEL_ENABLE=0 \
+	-DPTHREAD_CANCEL_DISABLE=1 \
+	-DPTHREAD_CANCELED=0
+LOCAL_SHARED_LIBRARIES += libbluetooth
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE := libcwiid
+include $(BUILD_SHARED_LIBRARY)
