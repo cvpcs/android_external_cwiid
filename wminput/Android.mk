071d4cf5fcbc6b31bf52388e151ad0718dd02fe7
cvpcs <root@cvpcs.org>
2011-01-14 19:01:41 -0600

    build changes for static wminput

--- wminput/Android.mk
+++ wminput/Android.mk
@@ -27,10 +27,11 @@ LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH)/../libcwiid \
 	$(LOCAL_PATH)/../libwminput
 LOCAL_CFLAGS += \
-	-DWMINPUT_CONFIG_DIR=\"/system/etc/cwiid/wminput\" \
-	-DCWIID_PLUGINS_DIR=\"/system/lib/cwiid/plugins\" \
+	-DWMINPUT_CONFIG_DIR=\"/sdcard/.cwiid/wminput\" \
+	-DCWIID_PLUGINS_DIR=\"/system/lib\" \
 	-DHAVE_CONFIG_H \
-	-DCWIID_STATE_SYSTEM_PROP=\"sys.cwiid.state\" \
+	-DCWIID_STATE_SYSTEM_PROP=\"sys.cwiid.status\" \
+	-DDEFAULT_CONFIG_FILE=\".autopreset\" \
 	-DCWIID_STATIC
 LOCAL_STATIC_LIBRARIES += libcwiid libwminput libcutils libm
 LOCAL_SHARED_LIBRARIES += libbluetooth
